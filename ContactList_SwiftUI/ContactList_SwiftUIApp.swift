b86228c916c4871b113d8b66b7dda244a60cbdd4
auhustsinovich <avgustinovich4@gmail.com>
2022-05-27 13:40:20 +0200

    Initial Commit

---
+++ ContactList_SwiftUI/ContactList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftUIApp.swift
+//  ContactList_SwiftUI
+//
+//  Created by Daniil Auhustsinovich on 27.05.22.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
