18c247ccd07980db735b6f2b3f247d8315dc4494
auhustsinovich <avgustinovich4@gmail.com>
2022-05-27 16:19:40 +0200

    Model settled up

---
+++ ContactList_SwiftUI/Person.swift
@@ -0,0 +1,52 @@
+//
+//  Person.swift
+//  ContactList_SwiftUI
+//
+//  Created by Daniil Auhustsinovich on 27.05.22.
+//
+
+import Foundation
+
+struct Person {
+    
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullname: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        
+        let iterationCount = min(
+            names.count,
+            surnames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
